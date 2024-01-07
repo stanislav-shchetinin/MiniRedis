f800c2f3b570606d026890bf6d59d984c2eb7ab0
Stas <stas.shc@gmail.com>
2024-01-07 17:48:35 +0300

    sуeparate in/out

--- src/Server/src/main.c
+++ src/Server/src/main.c
@@ -12,6 +12,8 @@
 #include <stdbool.h>
 #include <sys/epoll.h>
 #include "request.h"
+#include "network.h"
+#include "nonblock.h"
 
 #pragma comment(lib, "ws2_32.general")
 
@@ -58,6 +60,30 @@ static int epoll_initialization(int flags){
     return epollfd;
 }
 
+#define MAX_FD 200000
+
+struct Conn {
+    int fd;
+    size_t rbuf_size;
+    uint8_t rbuf[4 + MAX_MSG];
+    size_t wbuf_size;
+    size_t wbuf_sent;
+    uint8_t wbuf[4 + MAX_MSG];
+};
+
+struct Conn* new_conn(int fd){
+    struct Conn* conn = malloc(sizeof(struct Conn));
+    if (conn){
+        conn->fd = fd;
+        conn->rbuf_size = 0;
+        conn->wbuf_size = 0;
+        conn->wbuf_sent = 0;
+    } else {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+}
+
 int main(){
     //ipv4, tcp
     int server_fd = socket_initialization(AF_INET, SOCK_STREAM, 0);
@@ -69,12 +95,17 @@ int main(){
     listen_initialization(server_fd, SOMAXCONN);
     socklen_t address_len = sizeof(address);
 
+    fd_set_nb(server_fd);
+
     //epoll
     int epollfd = epoll_initialization(0);
-    struct epoll_event evt = {.events = EPOLLIN, .data.fd = server_fd};
+    struct epoll_event evt = {.events = EPOLLIN | EPOLLET, .data.fd = server_fd};
     epoll_ctl(epollfd, EPOLL_CTL_ADD, server_fd, &evt);
 
-    while (true) {
+    struct Conn* fd2conn[MAX_FD];
+
+    while (true){
+        evt.events = 0;
 
         int timeout = -1;
         errno = 0;
@@ -88,16 +119,58 @@ int main(){
         }
 
         if (evt.data.fd == server_fd){
+
             int client_fd = accept_initialization(server_fd,
                                                   (struct sockaddr *)&address,
                                                           &address_len);
-            struct epoll_event evt = {.events = EPOLLIN | EPOLLET, .data.fd = client_fd};
-            epoll_ctl(epollfd, EPOLL_CTL_ADD, client_fd, &evt);
+            fd2conn[client_fd] = new_conn(client_fd);
+            struct epoll_event evt_new = {.events = EPOLLIN | EPOLLOUT | EPOLLET, .data.fd = client_fd};
+            epoll_ctl(epollfd, EPOLL_CTL_ADD, client_fd, &evt_new);
+
         } else {
-            int32_t err = request(evt.data.fd);
-            if (err){
-                close(evt.data.fd);
-                break;
+            if ((evt.events & (EPOLLIN | EPOLLOUT)) == (EPOLLIN | EPOLLOUT)) {
+                printf("EPOLLIN | EPOLLOUT");
+
+                int len;
+                struct Conn* conn = fd2conn[evt.data.fd];
+                read(evt.data.fd, &len, HEADER_SIZE);
+                read(evt.data.fd, conn->rbuf, len);
+                conn->rbuf_size = HEADER_SIZE + len;
+
+                conn->rbuf[conn->rbuf_size] = '\0';
+                printf("Client says: %s", &conn->rbuf[HEADER_SIZE]);
+
+                memcpy(conn->wbuf, &len, HEADER_SIZE);
+                memcpy(&conn->wbuf[HEADER_SIZE], conn->rbuf, len);
+                conn->wbuf_size = HEADER_SIZE + len;
+
+                conn = fd2conn[evt.data.fd];
+                write(evt.data.fd, conn->wbuf, conn->wbuf_size);
+
+                conn->wbuf_size = 0;
+
+            } else if ((evt.events & EPOLLIN) == EPOLLIN){
+                printf("EPOLLIN");
+                int len;
+                struct Conn* conn = fd2conn[evt.data.fd];
+                read(evt.data.fd, &len, HEADER_SIZE);
+                read(evt.data.fd, conn->rbuf, len);
+                conn->rbuf_size = HEADER_SIZE + len;
+
+                conn->rbuf[conn->rbuf_size] = '\0';
+                printf("Client says: %s", &conn->rbuf[HEADER_SIZE]);
+
+                memcpy(conn->wbuf, &len, HEADER_SIZE);
+                memcpy(&conn->wbuf[HEADER_SIZE], conn->rbuf, len);
+                conn->wbuf_size = HEADER_SIZE + len;
+
+            } else if ((evt.events & EPOLLOUT) == EPOLLOUT) {
+                printf("EPOLLOUT");
+                struct Conn* conn = fd2conn[evt.data.fd];
+                write(evt.data.fd, conn->wbuf, conn->wbuf_size);
+
+                conn->wbuf_size = 0;
+
             }
         }
 
